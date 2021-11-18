24eb31178c5782d68606be5106c1def2854fdda5
nicoo <nicolaslercari@gmail.com>
2021-11-18 20:30:44 -0300

    remove commet

--- src/server/routes/validators/getTasksQueryValidator.js
+++ src/server/routes/validators/getTasksQueryValidator.js
@@ -2,7 +2,6 @@ const Joi = require("joi");
 
 module.exports.getTasksQueryValidator = Joi.object({
   quantity: Joi.number().integer().min(1).default(3),
-  // Your other parameters ...
   limit: Joi.number().integer(),
   page: Joi.number().integer(),
 });
